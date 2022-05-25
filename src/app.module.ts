ee5575cce931cca37a82e8a709c73fc2dcb1388e
kookh <kookh@hutom.co.kr>
2022-05-25 09:23:49 +0900

    feat : nestjs-rate-limiter 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_GUARD } from '@nestjs/core';
+import { RateLimiterModule, RateLimiterGuard } from 'nestjs-rate-limiter';
 
 @Module({
-  imports: [],
+  imports: [
+    //10초(duration) 동안 3번(points)을 초과해서 호출하면 429 상태코드 반환, 10초가 지나면 리셋되어서 다시 요청이 가능해짐
+    //duration 과 points 는 상황에 맞게 적절히 장성
+    RateLimiterModule.register({
+      keyPrefix: 'test',
+      points: 3,
+      duration: 10,
+      customResponseSchema: () => {
+        return { statusCode: '429', message: 'Request has been blocked' };
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: RateLimiterGuard,
+    },
+  ],
 })
 export class AppModule {}
